4ad49facc84b61e23f752c77ea0f61f9e4c32d36
Johan Zuluaga <johan16zulu@gmail.com>
2023-03-30 21:56:10 -0500

    implementacion hero

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import Hero from './src/components/Hero/Hero';
 import { NavigationContainer } from '@react-navigation/native';
 import {store} from './src/store/store'
 import { Provider } from 'react-redux';
-
+import MenuNavigation from './src/navigation/DrawNavigation/MenuNavigation'
+import Hero from './src/components/Hero/Hero';
+import Register from './src/components/Register/Register';
+import Index from './src/navigation/screens/Index';
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Hero/>
+        {/* <MenuNavigation/> */}
+        {/* <Hero/> */}
+        <Register/>
+        {/* <Index/> */}
       </NavigationContainer>
     </Provider>  
   );
--- src/navigation/DrawNavigation/MenuNavigation.jsx
+++ src/navigation/DrawNavigation/MenuNavigation.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import Home from '../screens/Home';
-import Register from '../screens/Register';
+import Index from '../screens/Index';
 import Login from '../screens/Login';
 
 const Menu = createDrawerNavigator()
@@ -10,8 +9,7 @@ const Menu = createDrawerNavigator()
 export default function MenuNavigation() {
   return (
     <Menu.Navigator>
-        <Menu.Screen name='Home' component={Home}/>
-        <Menu.Screen name='Register' component={Register}/>
+        <Menu.Screen name='Home' component={Index}/>
         <Menu.Screen name='Login' component={Login}/>
     </Menu.Navigator>
   )
